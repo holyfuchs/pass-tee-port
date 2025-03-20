62dbd7ca65152f1af98a7cda22329aeecabe6e99
Patrick Fuchs <email@patrick-fuchs.com>
2025-03-20 19:51:49 +0100

    add test to submit generated passport data

--- smart-contracts/src/PassTeePort.sol
+++ smart-contracts/src/PassTeePort.sol
@@ -1,20 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-using ECDSA for bytes32;
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract PassTeePort {
     
     mapping(address => bool) public signers;
     struct PassportData {
-        bool over18;
         string name;
+        bool over18;
     }
-    mapping(uint256 => PassportData) public passportData;
+    mapping(uint256 => address) public passportID_to_wallet;
+    mapping(address => PassportData) public wallet_to_passport;
 
     constructor() {}
 
-    event PassportDataVerified(PassportData);
+    event PassportDataVerified(address owner, address signer);
 
     function add_signer(bytes memory quoteBody) external {
         // verify the quoteBody
@@ -27,25 +28,37 @@ contract PassTeePort {
         string name;
     }
     
-    function verify(
+    function submit_passport_data(
         PassportDataID calldata _data,
         bytes calldata _signature
-    ) external view {
+    ) external {
         bytes32 data_hash = _hashPassportDataID(_data);
         address _signer = ECDSA.recover(data_hash, _signature);
 
         // need to manage to verify the enclave as signer
         // require(signers[_signer], "unrecognized signer");
-        emit PassportDataVerified(_data);
-        passportData[_data.id] = PassportData({
+        emit PassportDataVerified(msg.sender, _signer);
+        address previous_wallet = passportID_to_wallet[_data.id];
+        if (previous_wallet != address(0)) {
+            delete wallet_to_passport[previous_wallet];
+        }
+        passportID_to_wallet[_data.id] = msg.sender;
+        wallet_to_passport[msg.sender] = PassportData({
             over18: _data.over18,
             name: _data.name
         });
     }
 
+    // function get_passport_data(
+    //     address _wallet
+    // ) external view returns (PassportData memory) {
+    //     return wallet_to_passport[_wallet];
+    // }
+
+    // TODO make internal
     function _hashPassportDataID(
         PassportDataID calldata _data
-    ) internal pure returns (bytes32) {
+    ) public pure returns (bytes32) {
         return keccak256(
                     abi.encode(
                         _data.id,
--- smart-contracts/test/PassTeePort.t.sol
+++ smart-contracts/test/PassTeePort.t.sol
@@ -11,11 +11,27 @@ contract PassTeePortTest is Test {
         passTeePort = new PassTeePort();
     }
 
-    function test_Increment() public {
-        passTeePort.add_signer(bytes(""));
-    }
+    // function test_ValidatePassportData() public {
+    //     PassTeePort.PassportDataID memory pass = PassTeePort.PassportDataID(
+    //         {id: 1337, over18: true, name: "John Doe"}
+    //     );
+    //     passTeePort.submit_passport_data(pass, bytes(""));
+    // }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        passTeePort.add_signer(bytes(""));
+    function test_signPassportData() public {
+        (address alice, uint256 alicePk) = makeAddrAndKey("alice");
+        vm.startPrank(alice);
+        PassTeePort.PassportDataID memory pass = PassTeePort.PassportDataID(
+            {id: 1337, over18: true, name: "A****"}
+        );
+        bytes32 pass_hash = passTeePort._hashPassportDataID(pass);
+        console.logBytes32(pass_hash);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(alicePk, pass_hash);
+        bytes memory signature = abi.encodePacked(r, s, v);
+        passTeePort.submit_passport_data(pass, signature);
+
+        (string memory name, bool over18) = passTeePort.wallet_to_passport(alice);
+        assertEq(name, "A****");
+        assertEq(over18, true);
     }
 }
